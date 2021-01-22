5e3f53cbd760c2b102e4b66212dc03cc8ef34215
stdd01 <stdd01@mail.ru>
2021-01-23 00:38:08 +0400

    fix

--- hw32/src/com/company/Main.java
+++ hw32/src/com/company/Main.java
@@ -1,6 +1,11 @@
 package com.company;
 
 import java.io.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -24,25 +29,74 @@ public class Main {
         System.out.println(gamerProgress);
     }
 
-    static void zipFiles(GameProgress gamer) throws IOException {
-        String filenameTxt = gamer.getName() + ".txt";
-        String filename = "C:\\Users\\stdd0\\savegames\\" + filenameTxt;
-        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("C:\\Users\\stdd0\\savegames\\saves.zip"));
-            FileInputStream fis = new FileInputStream(filename);)
-        {
-            ZipEntry entry = new ZipEntry(filenameTxt);
-            zout.putNextEntry(entry);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            zout.write(buffer);
-            zout.closeEntry();
-        }
-        catch(Exception ex){
-            System.out.println(ex.getMessage());
+public static class ZipUtils {
+
+    private List<String> fileList;
+    private static final String OUTPUT_ZIP_FILE = "C:\\Users\\stdd0\\savegames\\saves.zip";
+    private static final String SOURCE_FOLDER = "C:\\Users\\stdd0\\savegames";
+    public ZipUtils() {
+        fileList = new ArrayList< String >();
+    }
+
+    public void zipIt(String zipFile) {
+        byte[] buffer = new byte[1024];
+        String source = new File(SOURCE_FOLDER).getName();
+        FileOutputStream fos = null;
+        ZipOutputStream zos = null;
+        try {
+            fos = new FileOutputStream(zipFile);
+            zos = new ZipOutputStream(fos);
+
+            System.out.println("Output to Zip : " + zipFile);
+            FileInputStream in = null;
+
+            for (String file: this.fileList) {
+                System.out.println("File Added into saves.zip : " + file);
+                ZipEntry ze = new ZipEntry(source + File.separator + file);
+                zos.putNextEntry(ze);
+                try {
+                    in = new FileInputStream(SOURCE_FOLDER + File.separator + file);
+                    int len;
+                    while ((len = in .read(buffer)) > 0) {
+                        zos.write(buffer, 0, len);
+                    }
+                } finally {
+                    in.close();
+                }
+            }
+
+            zos.closeEntry();
+            System.out.println("Saves.zip compressed");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                zos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public void generateFileList(File node) {
+        if (node.isFile()) {
+            fileList.add(generateZipEntry(node.toString()));
+        }
+
+        if (node.isDirectory()) {
+            String[] subNote = node.list();
+            for (String filename: subNote) {
+                generateFileList(new File(node, filename));
+            }
+        }
+    }
+
+    private String generateZipEntry(String file) {
+        return file.substring(SOURCE_FOLDER.length() + 1, file.length());
+    }
+}
+    public static void main(String[] args) throws Exception {
 
         GameProgress gamer1 = new GameProgress("Gamer1", 123, 23, 4, 23.5);
         GameProgress gamer2 = new GameProgress("Gamer2",124, 24, 5, 23.6);
@@ -53,10 +107,9 @@ public class Main {
         saveGame(path, gamer1);
         saveGame(path, gamer2);
         saveGame(path, gamer3);
-        zipFiles(gamer1);
-        zipFiles(gamer2);
-        zipFiles(gamer3);
-
+        ZipUtils appZip = new ZipUtils();
+        appZip.generateFileList(new File(ZipUtils.SOURCE_FOLDER));
+        appZip.zipIt(ZipUtils.OUTPUT_ZIP_FILE);
     }
 }
 
